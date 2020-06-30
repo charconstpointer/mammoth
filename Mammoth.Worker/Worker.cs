dd4f65e3c33a498cc56309ca9624af236eba25ba
charconstpointer <michalolszewskidev@gmail.com>
2020-06-30 15:08:04 +0200

    init

---
+++ Mammoth.Worker/Worker.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Mammoth.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            // var channel = GrpcChannel.ForAddress("http://localhost:5010");
+            var channel = GrpcChannel.ForAddress("http://api:5010");
+            var client = new Greeter.GreeterClient(channel);
+            
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await client.SayHelloAsync(new HelloRequest{Name = "foobar"});
+                    _logger.LogInformation("sent!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
