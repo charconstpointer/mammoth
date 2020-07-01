8246a3b4063aabb69b1b8552e24df39caf6c386a
charconstpointer <attwistingnether@gmail.com>
2020-07-01 14:36:24 +0200

    stack idea

---
+++ Mammoth.Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Mammoth.Core.Entities;
+
+namespace Mammoth.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var playlist = new Playlist();
+            playlist.AddChannel(1, new List<Track>
+            {
+                new Track("first"),
+                new Track("second"),
+                new Track("third"),
+            });
+            Console.WriteLine(playlist.GetCurrentlyPlayed(1).Title);
+            Console.WriteLine(playlist.GetCurrentlyPlayed(1).Title);
+            Console.WriteLine(playlist.GetCurrentlyPlayed(1).Title);
+        }
+    }
+}
